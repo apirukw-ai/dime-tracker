a9b6eccaabac89f9cbc50cd40fc0186462079632
apirukw-ai <apirukw@gmail.com>
2026-08-29 16:00:35 +0700

    Update update_prices.py

--- update_prices.py
+++ update_prices.py
@@ -2,14 +2,17 @@ import os
 import requests
 import json
 
-firebase_url = os.environ["FIREBASE_DB_URL"]
+firebase_url = (
+    os.environ["FIREBASE_DB_URL"]
+    + "/dime_ports/my-real-dime-port.json"
+)
 
 print("Loading Firebase...")
+print("URL =", firebase_url)
 
 response = requests.get(firebase_url)
 response.raise_for_status()
 
-print(repr(firebase_url))
 print("STATUS =", response.status_code)
 print("TEXT =", response.text[:500])
 
